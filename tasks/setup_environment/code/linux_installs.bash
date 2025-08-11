f2ef370334cfa3d09d63cda8341aa5fd32274774
JohnCRUf <johnruf@outlook.com>
2025-08-11 16:24:32 -0500

    added first draft of initialization script

---
+++ tasks/setup_environment/code/linux_installs.bash
@@ -0,0 +1,12 @@
+# Ubuntu
+sudo apt install texlive-latex-base
+sudo apt install texlive-latex-recommended
+sudo apt install texlive-science
+sudo apt install texlive-pictures
+sudo apt install texlive-latex-extra
+# Arch
+sudo pacman -S texlive-core
+# Fedora
+sudo dnf install texlive
+# macOS
+brew install --cask 
